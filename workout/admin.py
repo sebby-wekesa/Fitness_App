c7af42f398f495b58cb3f2d6a93a685763828b55
sebby-wekesa <sebbywakis@gmail.com>
2025-09-09 15:11:27 +0300

    modify admin.py file

--- workout/admin.py
+++ workout/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Workout, WorkoutPlan, WorkoutSession, MuscleGroup
 
-# Register your models here.
+class MuscleGroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "muscle_group", "demonstration_video_url")
+
+admin.site.register(MuscleGroup, MuscleGroupAdmin)
+admin.site.register(Workout)
+admin.site.register(WorkoutPlan)
+admin.site.register(WorkoutSession)
